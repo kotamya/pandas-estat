836f7e0db3aea2d9c5bbd273d2cb109b2df1b432
Shota Imaki <shota.imaki@icloud.com>
2020-11-30 12:47:00 +0900

    Refactor (close #42)

--- pandas_estat/statslist.py
+++ pandas_estat/statslist.py
@@ -8,8 +8,16 @@ from pandas_estat.base import BaseReader
 from pandas_estat.exceptions import EStatError
 
 
-
-def read_statslist(code, limit=None, updated_date=None, start_position=None, **kwargs):
+def read_statslist(
+    code,
+    limit=None,
+    start_position=None,
+    updated_date=None,
+    lang="J",
+    appid=None,
+    version="3.0",
+    **kwargs,
+):
     """
     統計表情報を取得します。
 
@@ -41,6 +49,17 @@ def read_statslist(code, limit=None, updated_date=None, start_position=None, **k
         * `YYYYMMDD`: 単日検索
         * `YYYYMMDD-YYYYMMDD`: 範囲検索
         e-Stat API の `updatedDate` に相当します。
+    - lang : {"J", "E"}, default "J"
+        取得するデータの言語です。
+        "J"（日本語）または "E"（英語）で指定してください。
+        e-Stat API の `lang` に対応します。
+    - appid : str, optional
+        アプリケーション ID です。
+        指定しない場合、`pandas_estat.set_appid(...)` で指定した値か、環境変数 `ESTAT_APPID` を用います。
+        e-Stat API の `appId` に対応します。
+    - version : str, default "3.0"
+        API 仕様バージョンです。
+        https://www.e-stat.go.jp/api/api-info/api-spec
     - **kwargs
         e-Stat API から取得した CSV データをパースする `pandas.read_csv` に与えるパラメータです。
 
@@ -49,7 +68,16 @@ def read_statslist(code, limit=None, updated_date=None, start_position=None, **k
     dataframe : pandas.DataFrame
         統計表情報
     """
-    dataframe = StatsListReader(code, limit=limit, updated_date=updated_date, start_position=start_position).read(**kwargs)
+    reader = StatsListReader(
+        code,
+        limit=limit,
+        start_position=start_position,
+        updated_date=updated_date,
+        lang=lang,
+        appid=appid,
+        version=version,
+    )
+    dataframe = reader.read(**kwargs)
     return dataframe
 
 
@@ -85,23 +113,17 @@ class StatsListReader(BaseReader):
         * `YYYYMMDD`: 単日検索
         * `YYYYMMDD-YYYYMMDD`: 範囲検索
         e-Stat API の `updatedDate` に相当します。
-    - version : str, default "3.0"
-        API 仕様バージョンです。
-        https://www.e-stat.go.jp/api/api-info/api-spec
     - lang : {"J", "E"}, default "J"
         取得するデータの言語です。
-        "J" (日本語) または "E" (英語) で指定してください。
-        `lang`
+        "J"（日本語）または "E"（英語）で指定してください。
+        e-Stat API の `lang` に対応します。
     - appid : str, optional
         アプリケーション ID です。
         指定しない場合、`pandas_estat.set_appid(...)` で指定した値か、環境変数 `ESTAT_APPID` を用います。
-        次のページから取得できます。
-        https://www.e-stat.go.jp/api/
-        `appId`
-
-    TODO
-    ----
-    * Fetch all rows by concatination
+        e-Stat API の `appId` に対応します。
+    - version : str, default "3.0"
+        API 仕様バージョンです。
+        https://www.e-stat.go.jp/api/api-info/api-spec
     """
 
     QUERY = "getSimpleStatsList"
@@ -113,17 +135,17 @@ class StatsListReader(BaseReader):
         limit=None,
         start_position=None,
         updated_date=None,
-        version="3.0",
         lang="J",
         appid=None,
+        version="3.0",
     ):
         self.code = code
         self.limit = limit
         self.start_position = start_position
         self.updated_date = updated_date
-        self.version = version
         self.lang = lang
         self.appid = get_appid(appid)
+        self.version = version
 
         if self.appid is None:
             raise ValueError("アプリケーション ID が指定されていません。")
@@ -134,12 +156,18 @@ class StatsListReader(BaseReader):
             )
 
         if lang != "J":
-            raise NotImplementedError  # TODO
+            raise NotImplementedError
 
     @property
     def params(self) -> dict:
         """
-        リクエスト URL に与えるパラメタ群
+        e-Stat API のパラメータ群を `dict` 形式で返します。
+        参照: e-Stat API v3.0 仕様 2.1 API の利用方法 - 統計表情報取得
+        参照: e-Stat API v3.0 仕様 3.2 API パラメータ - 統計表情報取得
+
+        Returns
+        -------
+        params : dict
         """
         params = {"appId": self.appid, "statsCode": self.code}
 
@@ -156,7 +184,7 @@ class StatsListReader(BaseReader):
 
     def read(self, **kwargs) -> pd.DataFrame:
         """
-        統計表を取得します。
+        e-Stat API から統計表情報を取得し、`pandas.DataFrame` 形式で返します。
 
         Parameters
         ----------
@@ -166,20 +194,17 @@ class StatsListReader(BaseReader):
         Returns
         -------
         dataframe : pandas.DataFrame
-            統計表
+            統計表情報
         """
         response = self.get()
         response_parsed = self._parse_response_text(response.text)
 
         if "TABLE" not in response_parsed:
-            msg = (
-                f'{response_parsed["ERROR_MSG"]}'
-                f'(STATUS: {response_parsed["STATUS"]})'
+            raise EStatError(
+                f'{response_parsed["ERROR_MSG"]} (STATUS: {response_parsed["STATUS"]})'
             )
-            raise EStatError(msg)
 
         if "dtype" not in kwargs:
-            # TODO better dtypes
             kwargs["dtype"] = str
 
         dataframe = pd.read_csv(io.StringIO(response_parsed["TABLE"]), **kwargs)
