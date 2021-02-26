fda7ed94963af4d7f94162ae522f329aed396691
Shota Imaki <shota.imaki.0801@gmail.com>
2021-02-26 12:14:38 +0900

    update test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,7 @@ python3 -m pytest --doctest-modules pandas_estat
 python3 -m pytest --doctest-modules tests
 
 python3 -m flake8 pandas_estat
-python3 -m black --check pandas_estat || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m black pandas_estat
-python3 -m isort --check --force-single-line-imports pandas_estat || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports pandas_estat
+python3 -m black --check --quiet pandas_estat || read -p "Run black? (y/n): " yn; [[ $yn = [yY] ]] && python3 -m black --quiet pandas_estat
+python3 -m isort --check --force-single-line-imports pandas_estat || read -p "Run isort? (y/n): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports --quiet pandas_estat
+python3 -m black --quiet tests
+python3 -m isort --force-single-line-imports --quiet tests
