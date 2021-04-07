1836a68f43cec724d3b414fbfcbf160d75b210ca
Shota Ray Imaki <shota.imaki.0801@gmail.com>
2021-04-07 20:56:30 +0900

    Issue/#85 (#86)
    * Delete .flake8
    
    * Update pyproject.toml
    
    * Update test.sh
    
    * Update ci.yml

--- test.sh
+++ test.sh
@@ -3,7 +3,6 @@
 python3 -m pytest --doctest-modules pandas_estat
 python3 -m pytest --doctest-modules tests
 
-python3 -m flake8 pandas_estat
 python3 -m black --check --quiet pandas_estat || read -p "Run black? (y/n): " yn; [[ $yn = [yY] ]] && python3 -m black --quiet pandas_estat
 python3 -m isort --check --force-single-line-imports pandas_estat || read -p "Run isort? (y/n): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports --quiet pandas_estat
 python3 -m black --quiet tests
