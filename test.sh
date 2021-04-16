26c681293e5480784bb357241c96a2cb117b1d74
Shota Ray Imaki <shota.imaki.0801@gmail.com>
2021-04-16 23:07:32 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,5 @@
 
 python3 -m pytest --doctest-modules pandas_estat
 python3 -m pytest --doctest-modules tests
-
-python3 -m black --check --quiet pandas_estat || read -p "Run black? (y/n): " yn; [[ $yn = [yY] ]] && python3 -m black --quiet pandas_estat
-python3 -m isort --check --force-single-line-imports pandas_estat || read -p "Run isort? (y/n): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports --quiet pandas_estat
-python3 -m black --quiet tests
-python3 -m isort --force-single-line-imports --quiet tests
+python3 -m black --quiet .
+python3 -m isort --force-single-line-imports --quiet .
