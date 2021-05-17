88cb1dff1d7445cc29580ce979742de6770507e1
simaki <shota.imaki.0801@gmail.com>
2021-05-17 12:06:30 +0900

    Release/0.2.8 (#98)
    * GITHUB: Create CODE_OF_CONDUCT.md (#95)
    
    * CHORE: Create Makefile (#93)
    
    * CHORE: Setup GitHub Action to Publish on Release (#94)
    
    * CHORE: Delete issue templates (#97)

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -eu
-
-python3 -m pytest --doctest-modules pandas_estat
-python3 -m pytest --doctest-modules tests
-python3 -m black --quiet .
-python3 -m isort --force-single-line-imports --quiet .
